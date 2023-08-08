61da6f2108ef8e77136c673af95acf73ebffa85e
Roman Kolobych <romankol1223@gmail.com>
2023-08-08 19:36:26 +0300

    end sets

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,4 +2,4 @@ export { default as isValidId } from "./isValidId.js";
 export { default as isBodyEmpty } from "./isBodyEmpty.js";
 export { default as isBodyFavoriteEmpty } from "./isBodyFavoriteEmpty.js";
 export { default as authenticate } from "./authenticate.js";
-// export { default as authenticate } from "./authenticate1.js";
+export { default as upload } from "./upload.js";
--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -5,12 +5,13 @@ import authController from "../../controllers/auth-controller.js";
 
 import usersSchemas from "../../schemas/users-schemas.js";
 
-import { authenticate } from "../../middlewares/index.js";
+import { upload, authenticate } from "../../middlewares/index.js";
 
 const authRouter = express.Router();
 
 authRouter.post(
   "/register",
+
   validateBody(usersSchemas.userSingupSchema),
   authController.signup
 );
@@ -24,5 +25,12 @@ authRouter.post(
 authRouter.post("/logout", authenticate, authController.logout);
 
 authRouter.get("/current", authenticate, authController.getCurrent);
+
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatarURL"),
+  authController.updateAvatar
+);
 
 export default authRouter;
